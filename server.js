9f08c37035c4d0374a3afc3dd86f35fa803d1173
Mada Gower <gmgower@gmail.com>
2019-11-01 21:56:47 -0500

    Create blog-router.js transfer endpoints to router & refactor endpoints.

--- server.js
+++ server.js
@@ -3,16 +3,23 @@
 // ? s2
 const express = require('express');
 
+// ? s20 import router
+const blogRouter = require('./blog-router')
+
 // ? s3
-const blogDB = require('./data/db');
+// ? 22 cut & paste to blog-router.js
+// const blogDB = require('./data/db');
 
 // ? s4
 const server = express();
 
-// ? s5
+// ? s5 middleware
 server.use(express.json());
 
-// ? s9 test on postman and broswer localhost:8000
+// ? s23 tell server to use router, set base url(route)
+server.use('/api/posts', blogRouter)
+
+// ? s9 test on postman and broswer localhost:8000 this is absolute pather
 server.get('/', (req, res) => {
   res.send(`
       <h2>Building RESTful APIs with Express</h>
@@ -20,196 +27,8 @@ server.get('/', (req, res) => {
     `);
 });
 
-// ! s13 POST request to /api/posts
-server.post('/api/posts', (req, res) => {
-  const postInfo = req.body;
-  const { title, contents } = req.body;
+//? s21 cut & paste all endpost with /api/post/ to blog-router.js
 
-  if (!title || !contents) {
-    res
-      .status(400)
-      .json({
-        success: false,
-        errorMessage: 'Please provide title and contents for the post.'
-      });
-  } else {
-    blogDB
-      .insert(postInfo)
-      .then(post => {
-        res.status(201).json({ success: true, post });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({
-            error: 'There was an error while saving the post to the database.'
-          });
-      });
-  }
-});
-
-// ! s14 POST request to /api/posts/:id/comments
-server.post('/api/posts/:id/comments', (req, res) => {
-  const id = req.params.id;
-  const newComment = req.body;
-  if (req.body.text) {
-    blogDB
-      .findById(id)
-      .then(comment => {
-        if (comment.length === 0) {
-          res
-            .status(404)
-            .json({
-              message: `The post with the specified ID ${id} does not exist.`
-            });
-        }
-        return comment;
-      })
-      .then(
-        blogDB
-          .insertComment(newComment)
-          .then(idObj => {
-            debug
-              .findCommentById(idObj.id)
-              .then(comment => {
-                res.status(201).json(comment);
-              })
-              .catch(error => {
-                res.status(500).json({ message: 'Error getting new comment' });
-              });
-          })
-          .catch(error => {
-            res
-              .status(500)
-              .json({
-                error:
-                  'These was an error while saving the comment to the database'
-              });
-          })
-      );
-  } else {
-    res
-      .status(400)
-      .json({ errorMessage: 'Please provide text for the comment.' });
-  }
-});
-
-// ! s10 GET request to /api/posts
-server.get('/api/posts', (req, res) => {
-  blogDB
-    .find()
-    .then(posts => {
-      res.status(200).json(posts);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: 'The posts information could not be retrieved.', err });
-    });
-});
-
-// ! s11 GET request to /api/posts/:id
-server.get('/api/posts/:id', (req, res) => {
-  const id = req.params.id;
-
-  blogDB
-    .findById(id)
-    .then(post => {
-      if (post) {
-        res.status(200).json({ success: true, post });
-      } else {
-        res
-          .status(404)
-          .json({
-            success: false,
-            message: `The post with the specified ID ${id} does not exist.`
-          });
-      }
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ err: `The user information could not be retrieved.`, err });
-    });
-});
-
-// ! s12 GET request to /api/posts/:id/comments
-server.get('/api/posts/:id/comments', async (req, res) => {
-  try {
-    const comments = await blogDB.findCommentById(req.params.id);
-
-    if (comments.length > 0) {
-      res.status(200).json(comments);
-    } else {
-      res
-        .status(404)
-        .json({
-          message: `The post with the specified ID ${id} does not exist.`
-        });
-    }
-  } catch (error) {
-    res.status(500).json({
-      error: 'The comments information could not be retrieved.'
-    });
-  }
-});
-
-// ! S16 PUT request to /api/posts/:id
-server.put('/api/posts/:id', (req, res) => {
-  const { id } = req.params;
-  const postInfo = req.body;
-  const { title, contents } = req.body;
-
-  if (!title || !contents) {
-    res
-      .status(400)
-      .json({
-        success: false,
-        errorMessage: 'Please provide title and contents for the post.'
-      });
-  } else {
-    blogDB
-      .update(id, postInfo)
-      .then(user => {
-        if (user) {
-          res.status(200).json({ success: true, user });
-        } else {
-          res
-            .status(404)
-            .json({
-              success: false,
-              message: `The post with the specified ID ${id} does not exist.`
-            });
-        }
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: 'The post information could not be modified.', err });
-      });
-  }
-});
-// ! s15 DELETE request to /api/posts/:id
-server.delete('/api/posts/:id', (req, res) => {
-  const { id } = req.params;
-
-  blogDB
-    .remove(id)
-    .then(deletePost => {
-      if (deletePost) {
-        res.status(204).end();
-      } else {
-        res
-          .status(404)
-          .json({
-            message: `The post with the specified ID ${id} does not exist.`
-          });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ error: 'The post could not be removed', err });
-    });
-});
 
 // ? s6
 module.exports = server;
