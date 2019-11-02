bff1fd725b4d153a22cf7db9a0325477aa71f0c8
Mada Gower <gmgower@gmail.com>
2019-11-01 21:12:36 -0500

    POST request to /api/posts/:id/comments.

--- server.js
+++ server.js
@@ -20,6 +20,59 @@ server.get('/', (req, res) => {
     `);
   });
   
+// ! s13 POST request to /api/posts
+server.post('/api/posts', (req, res) => {
+    const postInfo = req.body;
+    const {title, contents} = req.body;
+
+    if(!title || !contents) {
+        res.status(400).json({success: false, errorMessage: "Please provide title and contents for the post."})
+    } else {
+        blogDB.insert(postInfo)
+        .then(post => {
+            res.status(201).json({success: true, post})
+        })
+        .catch(err => {
+            res.status(500).json({error: "There was an error while saving the post to the database."})
+        })
+    }
+})
+
+// ! s14 POST request to /api/posts/:id/comments
+server.post('/api/posts/:id/comments',  (req, res) => {
+    const id = req.params.id;
+    const newComment = req.body;
+    if(req.body.text) {
+        blogDB.findById(id)
+        .then(comment => {
+            if(comment.length === 0) {
+                res.status(404).json({message: `The post with the specified ID ${id} does not exist.`})
+            }
+            return comment;
+        })
+        .then(
+            blogDB.insertComment(newComment)
+            .then(idObj => {
+                debug.findCommentById(idObj.id)
+                .then(comment => {
+                    res.status(201).json(comment)
+                })
+                .catch(error => {
+                    res.status(500).json({message: "Error getting new comment"})
+                }) 
+            })
+            .catch(error => {
+                res.status(500).json({error: "These was an error while saving the comment to the database"})
+            })
+        )
+    } else {
+        res.status(400).json({errorMessage: "Please provide text for the comment."})
+    }
+})
+
+
+// ! s15 DELETE request to /api/posts/:id
+
 // ! s10 GET request to /api/posts
 server.get('/api/posts', (req, res) => {
     blogDB.find()
@@ -63,8 +116,7 @@ server.get('/api/posts/:id/comments', async (req, res) => {
         res.status(500).json({
             error: "The comments information could not be retrieved."
         })
-    }
-    
+    }    
 })
 
 
